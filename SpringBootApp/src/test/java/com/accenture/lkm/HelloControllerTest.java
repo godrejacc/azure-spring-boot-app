284bfd3bb1639f3349b47d2d256f5016e37cafb6
godrejacc <gitikab@yahoo.co.in>
2021-01-06 11:38:31 +0530

    azure app1

---
+++ SpringBootApp/src/test/java/com/accenture/lkm/HelloControllerTest.java
@@ -0,0 +1,37 @@
+package com.accenture.lkm;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.client.match.ContentRequestMatchers;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@WebMvcTest(HelloController.class)
+public class HelloControllerTest {
+
+	@Autowired
+	  private MockMvc mvc;
+	
+	@Test
+	public void getAllEmployeesAPI() throws Exception 
+	{
+	  mvc.perform(MockMvcRequestBuilders
+	      .get("/")
+	      .accept(MediaType.APPLICATION_JSON))
+	      .andExpect(status().isOk())
+	      .andExpect(content().string("Hello From Spring Boot"));
+	}
+}
